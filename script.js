6e1e57113579de0841d48c9c91666f254d806e0a
nfillip <nick.m.fillip@gmail.com>
2023-09-02 14:21:18 -0600

    built localstorage, dispalytime, setcolor functions

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+//VARIABLES TAGGED TO HTML
+  var saveButton = $(".saveBtn");
+  var containerLarge = $(".container-lg");
+  var currentDay = $("#currentDay");
+
+//FUNCTIONS
+//function: add textarea values to local storage upon save button click
+function addToLocalStorage(){
+  var grabDivId = ($(this).parent().attr("id"));
+  var textAreaText = $(this).prev().val();
+  var storeTextObjects = {
+     text: textAreaText,
+     indexHour: grabDivId
+   }
+  if (localStorage.getItem("scheduling") !== null) {
+    var temp = JSON.parse(localStorage.getItem("scheduling"));
+    var hasText = false;
+    for (var x = 0; x<temp.length; x++){
+      if (temp[x].indexHour === grabDivId){
+        temp[x].text = textAreaText;
+        hasText = true;
+      }
+    }
+      if (hasText === false){
+        temp.push(storeTextObjects);
+    }
+    localStorage.setItem("scheduling", JSON.stringify(temp));
+   } else {
+      localStorage.setItem("scheduling" , JSON.stringify([storeTextObjects]));
+   }
+}
+
+//function: display time at top of screen. This is being rerun every seconds with set interval timer
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  currentDay.text(rightNow);
+}
+
+//function: displays storage items on page even on refresh
+function displayStorageItems(){
+  if (localStorage.getItem("scheduling") !== null){
+    var startingTexts = JSON.parse(localStorage.getItem("scheduling"));
+    for (var y = 0 ; y<startingTexts.length; y++){
+      var concater = "#" + startingTexts[y].indexHour;
+      $(concater).children("textarea").text(startingTexts[y].text);
+    }
+  }
+}
+
+//function: change colors as the time changes
+function colorSet() {
+  for(var x = 9; x<18; x++) {
+    j = x-9;
+    if(dayjs().hour() < x) {
+      console.log(1)
+      containerLarge.children().eq(j).addClass('future')
+    } else if (dayjs().hour() === x){
+      console.log(2)
+      containerLarge.children().eq(j).addClass('present')
+    } else {
+      console.log(3);
+      containerLarge.children().eq(j).addClass('past')
+    }
+  }
+}
+  
+//EVENT LISTENER
+saveButton.on("click", addToLocalStorage)
+  
+//FUNCTION CALLS
+    colorSet();
+    displayStorageItems();
+    displayTime();
+    setInterval(displayTime, 1000);
+    setInterval(colorSet, 5000);
+
 });
